9fa79bc6a4833dc2b724be25fbcaabf5e21b51f9
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-06-29 09:05:25 -0400

    Se modicarón los datos de la conexión, se agregarón los datos de la nueva bd sigepsi

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=uptcms',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=sigepsi',
     'username' => 'postgres',
     'password' => 'postgres',
     'charset' => 'utf8',
