008ba242b82a5d7be993585128ed2bf2de581b27
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-06-29 09:40:12 -0400

    Se creo el model de busqueda de comunidades con las nuevas caracteristicas

---
+++ models/ComunidadesSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Comunidades;
+
+/**
+ * ComunidadesSearch represents the model behind the search form of `app\models\Comunidades`.
+ */
+class ComunidadesSearch extends Comunidades
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id_comunidad', 'id_tipo_comunidad', 'telefono_contacto', 'id_parroquia', 'id_user', 'id_estatus'], 'integer'],
+            [['rif', 'nombre', 'persona_contacto', 'email', 'direccion'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Comunidades::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id_comunidad' => $this->id_comunidad,
+            'id_tipo_comunidad' => $this->id_tipo_comunidad,
+            'telefono_contacto' => $this->telefono_contacto,
+            'id_parroquia' => $this->id_parroquia,
+            'id_user' => $this->id_user,
+            'id_estatus' => $this->id_estatus,
+        ]);
+
+        $query->andFilterWhere(['ilike', 'rif', $this->rif])
+            ->andFilterWhere(['ilike', 'nombre', $this->nombre])
+            ->andFilterWhere(['ilike', 'persona_contacto', $this->persona_contacto])
+            ->andFilterWhere(['ilike', 'email', $this->email])
+            ->andFilterWhere(['ilike', 'direccion', $this->direccion]);
+
+        return $dataProvider;
+    }
+}
