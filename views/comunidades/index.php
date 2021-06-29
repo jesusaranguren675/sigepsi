da6fdb99bf7fd5abcca9027db2b7b3dcc61f1af4
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-06-29 09:57:28 -0400

    Se cambio el titulo del boton crear comunidad, además se agrego la estructura data table

--- views/comunidades/index.php
+++ views/comunidades/index.php
@@ -20,7 +20,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="comunidades-index">
 
     <p>
-        <?= Html::a('Create Comunidades', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Crear comunidad', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
@@ -46,6 +46,41 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
+
+    <div class="container-fluid">
+        <div class="card mb-4">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Título</th>
+                                <th>Estatus</th>
+                                <th>Estado</th>
+                                <th>Nombre</th>
+                                <th>Linea de Investigación</th>
+                                <th>Fecha Inicio</th>
+                                <th>Fecha Fin</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Título</th>
+                                <th>Problema</th>
+                                <th>Objetivo General</th>
+                                <th>Objetivos Especificos</th>
+                                <th>Fecha Inicio</th>
+                                <th>Fecha Fin</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 </div>
