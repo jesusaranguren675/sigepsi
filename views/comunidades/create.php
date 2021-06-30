8d89f3431ee2e3c174b77b6766f0f2c63fef4975
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-06-30 10:47:53 -0400

    Se agrego el cintillo y titulo

--- views/comunidades/create.php
+++ views/comunidades/create.php
@@ -5,13 +5,16 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Comunidades */
 
-$this->title = 'Create Comunidades';
+$this->title = 'REGISTRAR COMUNIDADES';
 $this->params['breadcrumbs'][] = ['label' => 'Comunidades', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="comunidades-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<div class="cintillo">
+		<?php echo Html::img('@web/imagenes/cintillo.svg'); ?>
+	</div>
+	<h3 class="title-dashboard"><?= Html::encode($this->title) ?></h3>
 
     <?= $this->render('_form', [
         'model' => $model,
