4f8b8908fb05e58ac8f3515990ef8e54c86e8657
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-06-30 14:09:07 -0400

    Se agregarón migas de pan para mejorar la experiencia del usuario

--- views/comunidades/create.php
+++ views/comunidades/create.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Comunidades */
@@ -10,11 +11,16 @@ $this->params['breadcrumbs'][] = ['label' => 'Comunidades', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="comunidades-create">
-
 	<div class="cintillo">
 		<?php echo Html::img('@web/imagenes/cintillo.svg'); ?>
 	</div>
-	<h3 class="title-dashboard"><?= Html::encode($this->title) ?></h3>
+	<nav style="--bs-breadcrumb-divider: '';" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= Url::toRoute('comunidades/index')?>">Comunidades</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Registrar Comunidades</li>
+		</ol>
+	</nav>
+	<h4 class="title-dashboard"><?= Html::encode($this->title) ?></h4>
 
     <?= $this->render('_form', [
         'model' => $model,
