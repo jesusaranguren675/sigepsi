f8c0c52d9bcf65d82f5a8476d5ebf1cbb33ef38b
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-07-02 19:46:39 -0400

    Se agregarón cambios en las cdn

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,7 +22,7 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/dataTables.bootstrap4.min.css', // Contiene estilos css de bootstrap
         'css/styles.css', // Contiene los estilos principales del sistema
-        'css/sweetalert2.min.css', // Contiene los estilos css de la librería SweetAlert2
+        //'css/sweetalert2.min.css', // Contiene los estilos css de la librería SweetAlert2
         'css/animate.min.css', // Contiene los estilos de la librería animate
         //'Bootstrap_material/css/mdb.min.css',
     ];
@@ -31,11 +31,11 @@ class AppAsset extends AssetBundle
         //'https://code.jquery.com/jquery-3.5.1.slim.min.js',
         'js/bootstrap.bundle.min.js',
         'js/scripts.js', //Contiene el script del menu del sistema
-        'js/Chart.min.js', //Contiene la libreria de graficas Chart.js
-        'js/chartprincipal.js',
+        //'js/Chart.min.js', //Contiene la libreria de graficas Chart.js
+        //'js/chartprincipal.js',
         'js/jquery.dataTables.min.js',  // Contiene script de la libreria DataTable
         'js/dataTables.bootstrap4.min.js', // Contiene script de bootstrap js
-        'assets/demo/datatables-demo.js', // Contiene ejemplos de DataTable
+        //'assets/demo/datatables-demo.js', // Contiene ejemplos de DataTable
     ];
     public $depends = [
         'yii\web\YiiAsset',
