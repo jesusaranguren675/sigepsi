a2e98454ab6449977650d3debb6b946c9c0982a1
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-07-03 13:47:43 -0400

    Se modifico el formulario de actualización de comunidades

--- views/comunidades/update.php
+++ views/comunidades/update.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Comunidades */
@@ -12,7 +13,25 @@ $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="comunidades-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<!-- Cintillo -->
+	<div class="cintillo">
+		<?php echo Html::img('@web/imagenes/cintillo.svg'); ?>
+	</div>
+	<!-- Fin Cintillo -->
+
+	<!-- Migas de pan -->
+	<nav style="--bs-breadcrumb-divider: '';" aria-label="breadcrumb">
+		<ol class="breadcrumb alert alert-warning">
+			<li class="breadcrumb-item"><a href="<?= Url::toRoute('comunidades/index')?>">Comunidades </a></li>
+
+			<li class="breadcrumb-item active" aria-current="page">Modificar Comunidad</li>
+		</ol>
+	</nav>
+	<!-- Fin Migas de Pan -->
+
+	<h3 class="title-dashboard">Modificar Comunidad</h3>
+
+    <h1><!-- <?= Html::encode($this->title) ?> --></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
