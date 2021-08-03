aea7d52465e2b46a4e2fb1ae17d49e27577053a9
Samuel Hernandez <samuelmoiseshernandezrojas@gmail.com>
2021-08-03 11:13:44 -0430

    Se creo el modulo productos

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;port=5432;dbname=sigepsi',
     'username' => 'postgres',
-    'password' => 'postgres',
+    'password' => 'usuario',
     'charset' => 'utf8',
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
---
+++ views/productos/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ProductosSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Productos';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="productos-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Productos', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id_producto',
+            'producto',
+            'id_carrera',
+            'id_trayecto',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
