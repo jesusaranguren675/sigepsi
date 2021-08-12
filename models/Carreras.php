9d15f40faa1a50f12bc9494d0eff91db199def91
Jesús Aranguren <jesusaranguren675@gmail.com>
2021-08-12 14:42:47 -0400

    Se creo el model carreras

--- models/Carreras.php
+++ models/Carreras.php
@@ -5,12 +5,18 @@ namespace app\models;
 use Yii;
 
 /**
- * This is the model class for table "public.carreras".
+ * This is the model class for table "carreras".
  *
  * @property int $id_carrera
  * @property string $carrera
  * @property int $id_procedencia
  * @property int $id_especialidad
+ *
+ * @property Especialidades $especialidad
+ * @property Procedencias $procedencia
+ * @property Estudiantes[] $estudiantes
+ * @property EstudiantesSiace[] $estudiantesSiaces
+ * @property LineasInvestigacion[] $lineasInvestigacions
  */
 class Carreras extends \yii\db\ActiveRecord
 {
@@ -19,7 +25,7 @@ class Carreras extends \yii\db\ActiveRecord
      */
     public static function tableName()
     {
-        return 'public.carreras';
+        return 'carreras';
     }
 
     /**
@@ -28,11 +34,10 @@ class Carreras extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id_carrera', 'carrera', 'id_procedencia'], 'required'],
-            [['id_carrera', 'id_procedencia', 'id_especialidad'], 'default', 'value' => null],
-            [['id_carrera', 'id_procedencia', 'id_especialidad'], 'integer'],
-            [['carrera'], 'string'],
-            [['id_carrera'], 'unique'],
+            [['carrera', 'id_procedencia', 'id_especialidad'], 'required'],
+            [['id_procedencia', 'id_especialidad'], 'default', 'value' => null],
+            //[['id_procedencia', 'id_especialidad'], 'integer'],
+            [['carrera'], 'string', 'max' => 250],
             [['id_especialidad'], 'exist', 'skipOnError' => true, 'targetClass' => Especialidades::className(), 'targetAttribute' => ['id_especialidad' => 'id_especialidad']],
             [['id_procedencia'], 'exist', 'skipOnError' => true, 'targetClass' => Procedencias::className(), 'targetAttribute' => ['id_procedencia' => 'id_procedencia']],
         ];
@@ -49,5 +54,55 @@ class Carreras extends \yii\db\ActiveRecord
             'id_procedencia' => 'Id Procedencia',
             'id_especialidad' => 'Id Especialidad',
         ];
+    }
+
+    /**
+     * Gets query for [[Especialidad]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEspecialidad()
+    {
+        return $this->hasOne(Especialidades::className(), ['id_especialidad' => 'id_especialidad']);
+    }
+
+    /**
+     * Gets query for [[Procedencia]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProcedencia()
+    {
+        return $this->hasOne(Procedencias::className(), ['id_procedencia' => 'id_procedencia']);
+    }
+
+    /**
+     * Gets query for [[Estudiantes]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEstudiantes()
+    {
+        return $this->hasMany(Estudiantes::className(), ['id_carrera' => 'id_carrera']);
+    }
+
+    /**
+     * Gets query for [[EstudiantesSiaces]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEstudiantesSiaces()
+    {
+        return $this->hasMany(EstudiantesSiace::className(), ['id_carrera' => 'id_carrera']);
+    }
+
+    /**
+     * Gets query for [[LineasInvestigacions]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getLineasInvestigacions()
+    {
+        return $this->hasMany(LineasInvestigacion::className(), ['id_carrera' => 'id_carrera']);
     }
 }
